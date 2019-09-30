6d2b9e099c596f0937925a3183b228d8d5997089
agiusk <agiusk@tipico.com>
2019-09-30 14:13:11 +0200

    setting up codeceptjs

---
+++ codecept.conf.js
@@ -0,0 +1,22 @@
+exports.config = {
+  tests: './*/*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost',
+      browser: 'chrome'
+    }
+  },
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone']
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'sdetassessment'
+}
